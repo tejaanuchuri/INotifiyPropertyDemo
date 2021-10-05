51fd044c77387da41cb575d93a78c435aa841ae1
tejaanuchuri0009@gmail.com <tejaanuchuri0009@gmail.com>
2021-10-06 00:17:34 +0530

    INotifiyPropertyChangedDemo

---
+++ INotifiyPropertyDemo/Model/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INotifiyPropertyDemo.Model
+{
+    class Person : INotifyPropertyChanged
+    {
+        public Person()
+        {
+            _fisrtname = "Nirav";
+            _lastname = "Daraniya";
+        }
+        private string _fisrtname;
+        public string FirstName
+        {
+            get { return _fisrtname; }
+            set
+            {
+                _fisrtname = value;
+                OnPropertyRaised("FirstName");
+                OnPropertyRaised("FullName");
+            }
+        }
+
+        private string _lastname;
+        public string LastName
+        {
+            get { return _lastname; }
+            set
+            {
+                _lastname = value;
+                OnPropertyRaised("LastName");
+                OnPropertyRaised("FullName");
+            }
+        }
+
+
+        private string _fullname;
+        public string FullName
+        {
+            get { return _fisrtname + " " + _lastname; }
+            set
+            {
+                _fullname = value;
+                OnPropertyRaised("FullName");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyRaised(string propertyname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+    }
+}
